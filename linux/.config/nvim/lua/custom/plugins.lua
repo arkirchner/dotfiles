6ac7ea5fdf94e064c0ad1214568b0f389bc7d897
Armin Kirchner <post.armin@gmail.com>
2024-01-04 02:55:09 +0900

    Nvim with Github Copilet

--- linux/.config/nvim/lua/custom/plugins.lua
+++ linux/.config/nvim/lua/custom/plugins.lua
@@ -5,6 +5,13 @@ local plugins = {
     "christoomey/vim-tmux-navigator",
     lazy = false,
   },
+  {
+    "zbirenbaum/copilot.lua",
+    lazy = false,
+    config = function()
+      require("copilot").setup({})
+    end,
+  },
 }
 
 return plugins
