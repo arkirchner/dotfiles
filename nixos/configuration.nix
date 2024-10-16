e2ea4231974bce4c3215f3975299ad732ad237f5
Armin Kirchner <post.armin@gmail.com>
2024-10-16 22:08:33 +0900

    NixOS with bash aliases

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -68,6 +68,20 @@
       kitty
     ];
 
+    home.shellAliases = {
+      vi = "nvim";
+      vim = "nvim";
+
+      gs = "git status ";
+      ga = "git add ";
+      gb = "git branch ";
+      gc = "git commit";
+      gd = "git diff";
+      go = "git checkout ";
+      gk = "gitk --all&";
+      gx = "gitx --all";
+    };
+
     programs.bash.enable = true;
 
     home.stateVersion = "24.05";
