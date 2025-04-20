151d0c405240dad379c77c06f30f5b9de039720e
Armin Kirchner <post.armin@gmail.com>
2025-04-20 16:50:05 +0900

    Switching to flakes in NixOS

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -2,13 +2,13 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, lib, ... }:
+{ config, pkgs, lib, inputs, ... }:
 
 {
   imports =
     [ # Include the results of the hardware scan.
       /etc/nixos/hardware-configuration.nix
-      <home-manager/nixos>
+      inputs.home-manager.nixosModules.default
     ] ++ (import ./system_programs);
 
   # Bootloader.
