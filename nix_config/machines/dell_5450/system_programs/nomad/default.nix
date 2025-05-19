9274af1f15725c52ad208af3100bce193a2e175d
Armin Kirchner <post.armin@gmail.com>
2025-05-19 16:46:00 +0200

    Nomad to allow the poseidon to run.

---
+++ nix_config/machines/dell_5450/system_programs/nomad/default.nix
@@ -0,0 +1,29 @@
+{ config, lib, pkgs, ... }:
+{
+  services.nomad = {
+    enable = true;
+    extraSettingsPlugins = [pkgs.nomad-driver-podman];
+
+    settings = {
+      client = {
+        enabled = true;
+        # TODO: I needed to create /var/lib/alloc_mounts for nomad to start ...
+        alloc_dir = "/var/lib/nomad/alloc_mounts";
+      };
+      server = {
+        enabled = true;
+        bootstrap_expect = 1;
+        default_scheduler_config = {
+          scheduler_algorithm = "spread";
+          memory_oversubscription_enabled = true;
+        };
+      };
+
+      plugin = [{
+        nomad-driver-podman = {
+          config = { };
+        };
+      }];
+    };
+  };
+}
