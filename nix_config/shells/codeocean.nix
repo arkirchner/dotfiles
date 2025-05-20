eb8ace6831abb0913fc9d2ea089a6431f24a977b
Armin Kirchner <post.armin@gmail.com>
2025-05-20 15:09:45 +0200

    Shell for codeocean development

---
+++ nix_config/shells/codeocean.nix
@@ -0,0 +1,30 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    ruby_3_4
+    postgresql_16
+    libffi
+    openssl
+    libxml2
+    libxslt
+    zlib
+    vips
+    wget
+    curl
+    gnumake
+    libyaml
+    shared-mime-info
+    icu
+    corepack_20
+  ];
+
+  shellHook = ''
+    export BUNDLE_PATH=$PWD/.bundle
+    export GEM_HOME=$PWD/.bundle
+    export PATH=$PWD/.bundle/bin:$PATH
+    export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath (with pkgs; [ icu vips libyaml postgresql_16 ])};
+    export RUBY_YJIT_ENABLE=1;
+    export FREEDESKTOP_MIME_TYPES_PATH="${pkgs.shared-mime-info}/share/mime/packages/freedesktop.org.xml"
+  '';
+}
