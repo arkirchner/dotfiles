14ce18baafb89373a22cb2252620c332503260da
Armin Kirchner <post.armin@gmail.com>
2025-06-09 10:51:46 +0200

    NixVIM with globals and options

--- nix_config/nixos_modules/nixvim/default.nix
+++ nix_config/nixos_modules/nixvim/default.nix
@@ -4,7 +4,23 @@
     enable = true;
 
     colorschemes.catppuccin.enable = true;
-    plugins.lualine.enable = true;
+
+    globals = {
+      mapleader = " ";
+    };
+
+    opts = {
+      number = true;
+      swapfile = false;
+      backspace = "indent,eol,start";
+      relativenumber = true;
+      tabstop = 2;
+      softtabstop = 2;
+      shiftwidth = 2;
+      expandtab = true;
+      autoindent = true;
+      wrap = false;
+    };
   };
 }
 
