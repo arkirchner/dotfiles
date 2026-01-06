3160ec4b6d3f8c8ded937d6aef6d500725439d85
Armin Kirchner <post.armin@gmail.com>
2026-01-06 16:16:14 +0100

    Enable docker support for arm builds.

--- nix_config/nixos_modules/podman/default.nix
+++ nix_config/nixos_modules/podman/default.nix
@@ -21,13 +21,41 @@
     docker = {
       enable = true;
       extraOptions = "--add-runtime=runsc=${pkgs.gvisor}/bin/runsc";
+
+      # enableOnBoot = true;
+
+      # daemon = {
+      #   settings = {
+      #     features = {
+      #       buildkit = true;
+      #     };
+      #   };
+      # };
     };
   };
 
   # Useful other development tools
   environment.systemPackages = with pkgs; [
+    # qemu_full
     dive # look into docker image layers
     podman-tui # status of containers in the terminal
     podman-compose # start group of containers for dev
   ];
+
+  # Required for docker buildx / ARM emulation
+  # boot.kernelModules = [ "binfmt_misc" ];
+  boot.binfmt.emulatedSystems = [
+    "aarch64-linux"
+  ];
+
+  systemd.services.qemu-user-static = {
+    description = "Register QEMU static binaries for Docker ARM emulation";
+    after = [ "docker.service" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.docker}/bin/docker run --rm --privileged multiarch/qemu-user-static --reset -p yes";
+      RemainAfterExit = true;
+    };
+  };
 }
