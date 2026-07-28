b8ba75b9a89c321785ee917e99a3a8e6607df2da
Armin Kirchner <post.armin@gmail.com>
2026-07-28 12:56:28 +0200

    Fix DNS problem in home network

--- nix_config/nixos_modules/vpn.nix
+++ nix_config/nixos_modules/vpn.nix
@@ -5,11 +5,21 @@
   ...
 }:
 {
+  services.resolved.enable = true;
   services.openvpn.servers = {
     cloudVPN  = {
       autoStart = false;
       config = '' config /home/armin/vpn/openhpicloud.conf '';
       updateResolvConf = true;
+
+      up = ''
+        resolvectl dns $dev 10.231.0.41 10.231.0.42 10.231.0.43
+        resolvectl domain $dev "~openhpi.cloud" "~openhpi.net" "~openhpicloud.de" "~xopic.de"
+      '';
+
+      down = ''
+        resolvectl revert $dev
+      '';
     };
   };
 }
